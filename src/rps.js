00f929da9aa99514f2caffb61d5f29c2423d98b9
Dave McKenna <davemckenna@Dave-McKennas-MacBook-Pro-2.local>
2011-12-24 01:25:42 -0500

    implemented basic game object creation

--- src/rps.js
+++ src/rps.js
@@ -1,5 +1,35 @@
 var RPS = function (){
 
+	var games = []
+
+	this.getGames = function(){
+		return games;
+	};
+
+	this.Game = function(id){
+
+    if (arguments.length <= 0 || typeof arguments[0] != 'string' ){
+      throw new Error('Game() takes exactly 1 string arg ');
+    }
+
+    if (/^\d+$/.test(arguments[0])){
+      throw new Error(
+        'Game() Argument can not be a number, must be string or number/string combo'
+      );
+    }
+    
+    if (games.hasOwnProperty(id)){
+      throw new Error('Game IDs must be unique');
+    } else {
+      games[id] = this;
+    }
+
+    this.getId = function(){
+    	return id;
+    }
+
+	};
+
 };
 
 exports.RPS = RPS;
--- test/gameTest.js
+++ test/gameTest.js
@@ -18,8 +18,71 @@ module.exports = testCase({
 
   "TC 1 - Game() - constructor": testCase({
 
-    "test constructor must take 1 arg, a game id of string+int combo": function(test) {
+    "test rps has a Game constructor": function(test) {
 
+        test.equal(typeof this.rps.Game, 'function');
+
+        test.done();
+
+    },
+
+    "test constructor must take 1 arg, a game id of string or  string+int combo": 
+      function(test) {
+
+        var that = this;
+
+        test.throws(
+          function() {
+            var game = new that.rps.Game();
+          },
+          Error
+        );
+
+        test.throws(
+          function() {
+            var game = new that.rps.Game('1');
+          },
+          Error
+        );        
+
+        test.done();
+
+    },
+
+    "test rps should have a getGames() method that returns all games belonging to this rps instance": 
+      function(test) {
+
+        test.equal(typeof this.rps.getGames(), 'object')
+
+        var game1 = new this.rps.Game('abc123');
+
+        test.equal(typeof this.rps.getGames()['abc123'], 'object')
+        test.equal(this.rps.getGames()['abc123'].getId(), 'abc123')
+
+        var game2 = new this.rps.Game('abc456');
+
+        test.equal(typeof this.rps.getGames()['abc456'], 'object')
+        test.equal(this.rps.getGames()['abc456'].getId(), 'abc456')       
+        
+        test.equal(Object.keys(this.rps.getGames()).length, 2 );
+
+
+        test.done();
+
+    },
+
+    "test games should not be allowed to have the same id": 
+      function(test) {
+
+        var that = this;
+
+        test.throws(
+          function(){
+            var game1 = new that.rps.Game('abc123');
+            var game2 = new that.rps.Game('abc123');
+          },
+          Error
+        )
         test.done();
 
     }
