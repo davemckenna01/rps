cd563cb69e39c15b8b77d050d603986ffb8cc0a1
davemckenna01 <davemckenna01@gmail.com>
2011-12-28 00:40:20 -0500

    files are now set up to dev and test

--- public/javascripts/src/rps.js
+++ public/javascripts/src/rps.js
@@ -1,33 +1,34 @@
-var rps = function() {
+var RPS = function() {
 
-return {
-
-	initCommon: function(){
-	},
+	this.HomePage = function(){
 		
-	initCreateBtn : function (){
-		$('#createGame').one('click', this.createGame);
-	},
+		this.initCreateBtn = function(){
+			$('#createGame').one('click', this.createGame);
+		};
 
-	createGame : function (e){
-		e.preventDefault();
+		this.createGame = function(e){
+			e.preventDefault();
 
-		$.ajax({
-			url: '/create',
-			type: 'POST',
-			dataType: 'json',
-			//context: some dom element,
-			complete: function(res, status){
-				if (res.status == 200) {
-					var data = $.parseJSON(res.responseText);
-					window.location.href='/game/' + data.id;
+			$.ajax({
+				url: '/create',
+				type: 'POST',
+				dataType: 'json',
+				//context: some dom element,
+				complete: function(res, status){
+					if (res.status == 200) {
+						var data = $.parseJSON(res.responseText);
+						window.location.href='/game/' + data.id;
+					}
 				}
-			}
-		});
+			});
 
 
-		return false;		
-	}
+			return false;		
+		};
+
+	};
+
 	
-}
-}();
+	
+
+};
---
+++ public/javascripts/test/gameTest.js
@@ -0,0 +1,11 @@
+TestCase("TC 1 - Game() constructor", {
+  setUp: function () {
+    //
+  },
+
+  "test should store functions": function () {
+    assertTrue(true);
+  },
+
+
+});
