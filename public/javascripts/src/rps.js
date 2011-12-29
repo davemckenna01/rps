9e3aa09e9ab8d542f75d265be65667849727129f
davemckenna01 <davemckenna01@gmail.com>
2011-12-29 00:31:11 -0500

    began Game constructor

--- public/javascripts/src/rps.js
+++ public/javascripts/src/rps.js
@@ -1,5 +1,7 @@
 var RPS = function() {
 
+	var gameCreated = false;
+
 	this.HomePage = function(){
 		
 		this.initCreateBtn = function(){
@@ -28,7 +30,21 @@ var RPS = function() {
 
 	};
 
-	
-	
+	this.Game = function(id){
+
+		if (gameCreated){
+			throw new Error('Only 1 Game may be instantiated.');
+		}
+
+    if (arguments.length <= 0 || typeof arguments[0] != 'string' ){
+      throw new Error('Game() takes exactly 1 string arg ');
+    }
+
+    if (!gameCreated){
+    	gameCreated = true;
+    }
+
+
+	};
 
 };
--- public/javascripts/test/gameTest.js
+++ public/javascripts/test/gameTest.js
@@ -1,10 +1,40 @@
 TestCase("TC 1 - Game() constructor", {
   setUp: function () {
-    //
+    this.rps = new RPS();
   },
 
-  "test should store functions": function () {
-    assertTrue(true);
+  "test constructor must take 1 arg, a game id": function () {
+
+    var that = this;
+
+    assertException(
+      function(){
+        var game = that.rps.Game();
+      },
+      'Error'
+    );
+
+    assertNoException(
+      function(){
+        var game = that.rps.Game('abc123');
+      }
+    );    
+
+  },
+
+  "test only one Game should ever be created": function () {
+
+    var that = this;
+
+    var game1 = that.rps.Game('abc123');
+
+    assertException(
+      function(){
+        var game2 = that.rps.Game('abc456');
+      },
+      'Error'
+    );
+
   },
 
 
