ab16152052db6a8229b308ce170ca7a1f3a1795d
davemckenna01 <davemckenna01@gmail.com>
2012-01-04 19:25:55 -0500

    scrapped everything, starting from scratch

--- public/javascripts/test/gameTest.js
+++ public/javascripts/test/gameTest.js
@@ -1,41 +1,11 @@
-TestCase("TC 1 - Game() constructor", {
+TestCase("TC 1 - ...", {
   setUp: function () {
-    this.rps = new RPS();
+    //
   },
 
-  "test constructor must take 1 arg, a game id": function () {
+  "...": function () {
 
-    var that = this;
-
-    assertException(
-      function(){
-        var game = that.rps.Game();
-      },
-      'Error'
-    );
-
-    assertNoException(
-      function(){
-        var game = that.rps.Game('abc123');
-      }
-    );    
-
-  },
-
-  "test only one Game should ever be created": function () {
-
-    var that = this;
-
-    var game1 = that.rps.Game('abc123');
-
-    assertException(
-      function(){
-        var game2 = that.rps.Game('abc456');
-      },
-      'Error'
-    );
-
-  },
+  }
 
 
 });
