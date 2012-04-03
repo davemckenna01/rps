d7b803e7a9b682d9261874780bf6566e7e0f1325
Dave McKenna <davemckenna01@gmail.com>
2012-04-03 02:08:14 -0400

    herokuifying files

--- web.js
+++ web.js
@@ -55,7 +55,7 @@ app.get('/game/:id', function(req, res){
     res.render('game', {});
 });
 
-app.listen(29384);
+app.listen(3000);
 
 console.log("Express server listening on port %d in %s mode", 
             app.address().port, app.settings.env);
